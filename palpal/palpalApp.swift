58c44ff6f7f4a330b3ca50df48ef3d2ecfa3ce19
adam.tokarczyk <adam.tokarczyk@kyotutechnology.com>
2024-01-31 12:37:16 +0100

    Initial Commit

---
+++ palpal/palpalApp.swift
@@ -0,0 +1,17 @@
+//
+//  palpalApp.swift
+//  palpal
+//
+//  Created by Adam Tokarczyk on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct palpalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
